5b46372cd9c7b6a728f20fc2ddf30b1d238b7633
presori <presori@boom.al>
2026-08-08 19:16:24 +0200

    Unit cost language support and minor fixes

--- api/realEstate/private/unit/unitCost/createUnitCost.form.validator.ts
+++ api/realEstate/private/unit/unitCost/createUnitCost.form.validator.ts
@@ -12,10 +12,10 @@ import {
 export function unitCostFormSchema(languageCode: string, form: any = null) {
     return withTableFormValidator(languageCode, form, {
         id: isObjectIdZod(form?.["idLabel"] || "id", languageCode).optional(),
-        unitId: isObjectIdZod(form?.["unitIdLabel"] || "unitId", languageCode).optional(),
-        projectId: isObjectIdZod(form?.["projectIdLabel"] || "projectId", languageCode).optional(),
-        edificeId: isObjectIdZod(form?.["edificeIdLabel"] || "edificeId", languageCode).optional(),
-        floorId: isObjectIdZod(form?.["floorIdLabel"] || "floorId", languageCode).optional(),
+        unit: isObjectIdZod(form?.["unitLabel"] || "unit", languageCode).optional(),
+        project: isObjectIdZod(form?.["projectLabel"] || "project", languageCode).optional(),
+        edifice: isObjectIdZod(form?.["edificeLabel"] || "edifice", languageCode).optional(),
+        floor: isObjectIdZod(form?.["floorLabel"] || "floor", languageCode).optional(),
         verificationStatus: z.string().optional(),
         paymentStatus: z.string().optional(),
         tag: z.string().optional(),
